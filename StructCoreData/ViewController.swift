5f00eb9312d03f5505e8cd3a307b7369e4312f75
tt301 <tiantong301@gmail.com>
2019-04-12 20:42:00 +0800

    Initial Commit

---
+++ StructCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StructCoreData
+//
+//  Created by Tian Tong on 2019/4/12.
+//  Copyright © 2019 Tian Tong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
